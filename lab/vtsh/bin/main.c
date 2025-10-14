0258ba8ffef209812a399fe43d1d7285b0057555
soniina <sonikpav@gmail.com>
2025-10-14 21:51:47 +0300

    refactor: replace usleep with waitpid in handle_exit

--- lab/vtsh/bin/main.c
+++ lab/vtsh/bin/main.c
@@ -31,7 +31,7 @@ typedef struct {
   int background;
 } ChildProcessData;
 
-void redirect_std_to_null() {
+void redirect_background_std() {
   int null_fd = open("/dev/null", O_RDONLY);
   if (null_fd != -1) {
     dup2(null_fd, STDIN_FILENO);
@@ -39,8 +39,8 @@ void redirect_std_to_null() {
   }
   null_fd = open("/dev/null", O_WRONLY);
   if (null_fd != -1) {
-    dup2(null_fd, STDOUT_FILENO);
-    dup2(null_fd, STDERR_FILENO);
+    dup2(null_fd, STDOUT_FILENO);  // мб в файл
+    dup2(null_fd, STDERR_FILENO);  // мб в файл
     close(null_fd);
   }
 }
@@ -49,7 +49,7 @@ static int child_exec(void* arg) {
   ChildProcessData* data = (ChildProcessData*)arg;
 
   if (data->background) {
-    redirect_std_to_null();
+    redirect_background_std();
   }
 
   execvp(data->args[0], data->args);
@@ -85,22 +85,25 @@ int parse_command(char* input, char** args) {
   return background;
 }
 
+void remove_background_process(BackgroundProcesses* bg_procs, pid_t pid) {
+  for (int i = 0; i < bg_procs->count; i++) {
+    if (bg_procs->pids[i] == pid) {
+      free(bg_procs->stacks[i]);
+      bg_procs->pids[i] = bg_procs->pids[bg_procs->count - 1];
+      bg_procs->stacks[i] = bg_procs->stacks[bg_procs->count - 1];
+      bg_procs->count--;
+      break;
+    }
+  }
+}
+
 void check_background_processes(BackgroundProcesses* bg_procs) {
   int status = 0;
   pid_t pid = 0;
 
   while ((pid = waitpid(-1, &status, WNOHANG)) > 0) {
     printf("[%d] завершен\n", pid);
-
-    for (int i = 0; i < bg_procs->count; i++) {
-      if (bg_procs->pids[i] == pid) {
-        free(bg_procs->stacks[i]);
-        bg_procs->pids[i] = bg_procs->pids[bg_procs->count - 1];
-        bg_procs->stacks[i] = bg_procs->stacks[bg_procs->count - 1];
-        bg_procs->count--;
-        break;
-      }
-    }
+    remove_background_process(bg_procs, pid);
   }
 }
 
@@ -152,9 +155,13 @@ int execute_external_command(
 
 void handle_exit(BackgroundProcesses* bg_procs) {
   while (bg_procs->count > 0) {
-    printf("Ожидание завершения фоновых процессов...\n");
-    usleep(MICROSECONDS_PER_SECOND);
-    check_background_processes(bg_procs);
+    int status = 0;
+    pid_t pid = waitpid(-1, &status, 0);
+    if (pid > 0) {
+      remove_background_process(bg_procs, pid);
+    } else {
+      break;
+    }
   }
 }
 
